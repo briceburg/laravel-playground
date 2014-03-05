9b552c99e2bd5b3a13bef320b14c9d5febe27efc
Brice Burgess <briceburg@gmail.com>
2014-03-04 22:40:12 -0600

    environmentalize application config

--- todo-app/.env.sample.php
+++ todo-app/.env.sample.php
@@ -1,7 +1,13 @@
 <?php
 
 return array(
-	'MYSQL_USER' => 'root',
+    // APPLICATION SETUP
+    'APP_URL' => 'http://localhost/laravel-playground/todo-app',
+    'APP_SECRET' => 'TxHaBeytt86lvjsAoiWvUn6AoDviBpKd',
+    'APP_DEBUG' => true, // ensure false in production!
+
+    // DATABASE CREDENTIALS
+    'MYSQL_USER' => 'root',
     'MYSQL_PASS' => '',
     'MYSQL_HOST' => 'localhost',
     'MYSQL_DB' => 'local_laravel'
